9bb377183f92abb831dafe6e7ceb141950fb16a9
Suragch <asit.ghosh6@gmail.com>
2017-03-06 21:03:58 +0530

    6-3-17

---
+++ AdditionalSkillCell.swift
@@ -0,0 +1,26 @@
+//
+//  AdditionalSkillCell.swift
+//  MyPerfectTrainer
+//
+//  Created by Amar Banerjee on 06/03/17.
+//  Copyright © 2017 Ogma Conceptions. All rights reserved.
+//
+
+import UIKit
+
+class AdditionalSkillCell: UITableViewCell {
+
+    @IBOutlet var CrossBtn: UIButton!
+    @IBOutlet var name_lbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TrainerCreateController.swift
+++ TrainerCreateController.swift
@@ -9,20 +9,107 @@
 import UIKit
 import Alamofire
 import SVProgressHUD
-class TrainerCreateController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+class TrainerCreateController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate {
+    
+  
+    @IBOutlet var Certificate_tableview: UITableView!
+    @IBOutlet var CertificateView: UIView!
+    @IBOutlet var Additional_Skill_view: UIView!
+    @IBOutlet var Additional_skill_btn: UIButton!
+    @IBOutlet var Additional_skill_text: UITextField!
     @IBOutlet var skill_collectionview: UICollectionView!
+    @IBOutlet var Skill_table: UITableView!
     var arrskill = [[String:AnyObject]]()
+    var ArradditionalSkill = [String]()
+     var arrCertificateSkill = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     // self.CallFetchStates()
+        
         self.JsonForSkill()
+        Skill_table.isHidden=true
+        Skill_table.frame=CGRect.init(x: Additional_Skill_view.frame.origin.x-10, y: Additional_Skill_view.frame.origin.y+Additional_Skill_view.frame.size.height, width: Additional_Skill_view.frame.size.width/2, height: 0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func ViewReCoordination()  {
+        if arrCertificateSkill.isEmpty&&ArradditionalSkill.isEmpty==true {
+             CertificateView.frame=CGRect.init(x: Additional_Skill_view.frame.origin.x, y: Additional_Skill_view.frame.origin.y+Additional_Skill_view.frame.size.height, width: Additional_Skill_view.frame.size.width, height: Additional_Skill_view.frame.size.height)
+        }
+    }
+    
+    func UITableView_Auto_Height()
+    {
+        if(self.Skill_table.contentSize.height < self.Skill_table.frame.height){
+            var frame: CGRect = self.Skill_table.frame;
+            frame.size.height = self.Skill_table.contentSize.height;
+            self.Skill_table.frame = frame;
+        }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+     //  UITableView_Auto_Height();
+    }
+    
+   
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // For Tableview+++++++++++++++++++++++++++++++++++
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row == Skill_table.indexPathsForVisibleRows?.last?.row {
+            print("\(tableView.contentSize.height)")
+            Skill_table.isHidden=false
+            Skill_table.frame=CGRect.init(x: Additional_Skill_view.frame.origin.x, y: Additional_Skill_view.frame.origin.y+Additional_Skill_view.frame.size.height, width: Additional_Skill_view.frame.size.width, height: Skill_table.contentSize.height)
+            
+        }
+        if indexPath.row == Certificate_tableview.indexPathsForVisibleRows?.last?.row {
+            print("\(tableView.contentSize.height)")
+            Skill_table.isHidden=false
+            Certificate_tableview.frame=CGRect.init(x: CertificateView.frame.origin.x, y: CertificateView.frame.origin.y+CertificateView.frame.size.height, width: CertificateView.frame.size.width, height: Certificate_tableview.contentSize.height)
+            
+        }
+
+    }
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView==Skill_table {
+            return ArradditionalSkill.count
+        }
+        else{
+         return arrCertificateSkill.count
+        }
+       
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if tableView==Skill_table {
+            let cell : AdditionalSkillCell! = tableView.dequeueReusableCell(withIdentifier: "AdditionaleSkill") as! AdditionalSkillCell
+            cell.selectionStyle = UITableViewCellSelectionStyle.none
+            cell.name_lbl.text=(ArradditionalSkill[indexPath.row] as AnyObject) as? String
+            return cell
+        }
+        else{
+            let cell : AdditionalSkillCell! = tableView.dequeueReusableCell(withIdentifier: "CertificateSkill") as! AdditionalSkillCell
+            cell.selectionStyle = UITableViewCellSelectionStyle.none
+            cell.name_lbl.text=(ArradditionalSkill[indexPath.row] as AnyObject) as? String
+            return cell
+        }
+      
+    }
+    
+    
+
+    
     
 // For collection view+++++++++++++++++++++++++++++
     
@@ -45,6 +132,7 @@ class TrainerCreateController: UIViewController,UICollectionViewDataSource,UICol
     
     
     
+  // WebService Calling++++++++++++++++++++++++++++++++++++
     
     func JsonForSkill () {
         let url:String=Constants.Base_url+"skills"
@@ -80,7 +168,28 @@ class TrainerCreateController: UIViewController,UICollectionViewDataSource,UICol
  
     }
     
+    @IBAction func DidTabAdditionalSkillBtn(_ sender: Any) {
+        let arrayobject:String=Additional_skill_text.text!
+   ArradditionalSkill.append(arrayobject)
+        print("new array",ArradditionalSkill)
+     //   self.UITableView_Auto_Height()
+        Skill_table .reloadData()
+        
 
+    }
+
+    @IBAction func DidTabCrossBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func DidTabCertificateCellBtn(_ sender: Any) {
+        let arrayobject:String=Additional_skill_text.text!
+        ArradditionalSkill.append(arrayobject)
+        print("new array",ArradditionalSkill)
+        //   self.UITableView_Auto_Height()
+        Skill_table .reloadData()
+ 
+    }
     /*
     // MARK: - Navigation
 
